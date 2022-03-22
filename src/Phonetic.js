412617fb306242f7ac42f85ff184a7cc5b74a105
Bettycodes <bettygcodes@gmail.com>
2022-03-22 09:53:41 +0300

    Update Phonetic.js
    added a rel link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetic(props) {
     return(
         <div className="Phonetic">
-        <a href={props.Phonetic.audio} target ="_blank">Listen</a>
+        <a href={props.Phonetic.audio} target ="_blank" rel="noreferrer">Listen</a>
         {props.Phonetic.text}
         </div>
     )
